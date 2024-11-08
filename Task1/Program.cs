e1aa4c8f827c33fb7dd3ae221a43b3264bf85bea
Emmanuel Michael Ikechukwu <162467554+Emmanuel-Michael05@users.noreply.github.com>
2024-11-08 14:01:24 +0100

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring and initializing 
+            string name = "John Doe";
+
+            int age = 25;   
+
+            bool isAdmin = true;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age); 
+            Console.WriteLine(isAdmin); 
+
+            Console.ReadKey();
+        }
+    }
+}
